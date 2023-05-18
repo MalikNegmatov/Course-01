d4983a307eb4b0b92d30c5530e073fed184357e9
MalikNegmatov <maliknegmatov@gmail.com>
2023-05-18 19:58:31 +0300

    1. Создать класс Employee, который содержит информацию о ФИО, отделе и зарплате сотрудника. Отделы для простоты должны быть названы от 1 до 5.

---
+++ src/Employee.java
@@ -0,0 +1,7 @@
+public class Employee {
+    private String firstName;   // Имя
+    private String middleName;  // Отчество
+    private String lastName;    // Фамилия
+    private int department;     // отдел (1..5)
+    private int salary;         // зарплата
+}

053590471f4a5e5623b4d96396cf46e47b215e2d
MalikNegmatov <maliknegmatov@gmail.com>
2023-05-18 20:15:52 +0300

    5. Добавить возможность устанавливать значения полей отдела и зарплаты (сеттеры).

--- src/Employee.java
+++ src/Employee.java
@@ -29,4 +29,20 @@ public class Employee {
     public int getEmployeeID() {
         return salary;
     }
+    public void setDepartment(int department) {
+        if ( checkDepartment(department) ) {
+            this.department = department;
+        }
+        else System.out.println("Значение отдела должно быть от 1 до 5");
+    }
+    public void setSalary(int salary) {
+        if ( salary > 0 )
+            this.salary = salary;
+        else System.out.println("Зарплата не может быть отрицательной");
+    }
+
+    // Проверки на ошибки
+    private boolean checkDepartment(int department) {
+        return ( department > 0 && department < 6 );
+    }
 }

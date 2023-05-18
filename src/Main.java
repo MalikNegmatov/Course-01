cc974c8412b03133e9350d131fc245d5d8eba632
MalikNegmatov <maliknegmatov@gmail.com>
2023-05-18 20:46:23 +0300

    7. Создать внутри класса с методом main поле типа Employee[10], которое будет выполнять роль «хранилища» для записей о сотрудниках.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    public static Employee[] eList = new Employee[10];
     public static void main(String[] args) {
         Employee e = new Employee("василий", "петрович", "сидоров", 6, -119);
         System.out.println(e.getDepartment());
@@ -6,5 +7,10 @@ public class Main {
         System.out.println(e.getDepartment());
         System.out.println(e.getSalary());
         e.setSalary(323);
+
+        eList[0] = new Employee("Арсений", "Петрович", "сказка", 2, 203912);
+        System.out.println(eList[0]);
+        System.out.println(eList[1]);
     }
+
 }
